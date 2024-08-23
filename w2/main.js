3b4e3dbf630a7d07ba030ccfade7d06736973c47
jacobp <perezjacob527@gmail.com>
2024-08-22 20:20:06 -0700

    coding is done

--- w2/main.js
+++ w2/main.js
@@ -2,24 +2,22 @@
 // Method 1 
 
 // 1. Count the members of your household.
-const myHouseMembers = 14; 
+const myHouseMembers = 8; 
 // 2. Consider the size of your home
-const myHouseSize = 10;
+const myHouseSize = 7;
 // 3. Evaluate your food choices
-const mydomesMeatConsump = 10;
+const mydomesMeatConsump = 8;
 // 4. Examine your water comsumption
-const mydishWasherRun = 9;
+const mydishWasherRun = 2;
 // 5. Determine how many household purchases you make each year.
 const myHouseHoldpurch = 8;
 // 6. Consider how much waste you produce
-const myWasteProd = 50;
+const myWasteProd = 40;
 // 7. Identify the amount of waste that you recycle
-const myRecylWaste = 24;
+const myRecylWaste = 22;
 // 8. Tally up your annual transportation scores
 const myTransScore = 12;
 // 9. Add up your points
 const cfpTotal = myHouseMembers + myHouseMembers + myHouseSize + myRecylWaste + myTransScore + myWasteProd + mydishWasherRun + mydomesMeatConsump
-// 10. Write JS to update the rendered html (index.html) with total
-
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "My Total";
+const myHeading = document.querySelector("h2");
+myHeading.textContent = "107";
