6dc0a46103b9cff8b02209faafe52c8395794e97
jacobp <perezjacob527@gmail.com>
2024-08-26 20:16:05 -0700

    week 3 attendance

--- w1/main.js
+++ w1/main.js
@@ -1,1 +1,23 @@
-console.log('Hello World!')
+console.log("Hello from inside the main.js file");
+
+//let myVar;
+
+const myVar = "jacob";
+const myVarType = typeof(myVar);
+console.log("myVarType " + myVarType);
+console.log(`myVarType + ${myVarType}`);
+
+
+
+function runNow () {
+    if(myVarType === "number") {                                          
+        console.log(`will 11 this one run?`);
+    } else {
+        console.log(`will this 13 run?`);
+    }
+}
+
+runNow();
+runNow();
+
+
