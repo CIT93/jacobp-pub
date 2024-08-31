6efb309aee9c595fd3bdd5c04954a840583fc67b
jacobp <perezjacob527@gmail.com>
2024-08-30 21:09:53 -0700

    Working Code

--- w3/main.js
+++ w3/main.js
@@ -21,10 +21,29 @@ function determineHouseHoldPts(numberInHousehold) {
 let carbonFootprintpoints = 0;
 //const numberInHousehold = 3;
 
-
 // global scope
-
-
 
 determineHouseHoldPts(3)
 determineHouseHoldPts(4)
+
+function dtrmHousesize(houseSize) {
+    console.log("Inside the function")
+if (houseSize === "Large House") {
+    housePoints = housePoints + 10;
+} else if (houseSize === "Medium-Sized House") {
+    housePoints = housePoints + 7;
+} else if (houseSize === "Small House") {
+    housePoints = housePoints + 4;
+} else if (houseSize === "Apartment") {
+    housePoints = housePoints + 2;
+}
+console.log(`Based on the size of the ${ houseSize}  the points would be ${ housePoints } `)
+}
+
+let housePoints = 0;
+
+dtrmHousesize("Large House")
+dtrmHousesize("Medium-Sized House")
+
+
+
